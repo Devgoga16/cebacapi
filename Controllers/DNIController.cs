655702fd123b1eaa0d7c08227f5f9d68c37248ad
cizquierdo-u <cizquierdo@unify-tec.com>
2025-03-22 21:53:32 -0500

    first commit

---
+++ Controllers/DNIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using ApiModel.General;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace UnifyAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DNIController : ControllerBase
+    {
+        public DNIController()
+        {
+        }
+
+        [HttpGet]
+        [Route("consultDocument/{documentNumber}")]
+        public IActionResult ConsultDocument(string documentNumber)
+        {
+            try
+            {
+                var client = new RestClient("https://api.apis.net.pe/v1/dni?numero=" + documentNumber);
+                var request = new RestRequest();
+
+                request.Method = Method.Get;
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", "Bearer apis-token-3781");
+
+                var apiReponse = client.Execute(request);
+                var statusResponse = apiReponse.StatusDescription;
+
+                if (statusResponse == "Not Found")
+                {
+                    throw new Exception("NotFoundDocument");
+                }
+                else
+                {
+                    var JsonApiResponse = JsonSerializer.Deserialize<ResponseApiDNI>(apiReponse.Content ?? "" );
+
+                    return Ok(JsonApiResponse);
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ResponseApiDNI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiModel.General
+{
+    public class ResponseApiDNI
+    {
+        public string? tipoDocumento { get; set; }
+        public string? numeroDocumento { get; set; }
+        public string? apellidoPaterno { get; set; }
+        public string? apellidoMaterno { get; set; }
+        public string? nombres { get; set; }
+    }
+}
